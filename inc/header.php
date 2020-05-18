4fcd70990132b05fa13ad689d98b227351c16f9a
tang1005 <59104160+tang1005@users.noreply.github.com>
2020-05-18 18:35:41 +0800

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <?php wp_head(); ?>
+    <?php if( dopt('d_headcode_b') != '' ) echo dopt('d_headcode'); ?>
+</head>
+
+<body class="nav-open">
+<?php
+    $home_url = esc_url( home_url() ) ;
+
+    if( is_mobile() ) {
+    	echo '<div id="mobile-nav" class="f12 yahei"><form class="mm-search" action="'.$home_url.'" method="get" role="search"><input type="text" autocomplete="off" placeholder="Search" name="s" value=""><input id="mobilesubmit" type="submit" value="Search"></form> ';
+    	if(function_exists('wp_nav_menu')) {
+    		wp_nav_menu(array( 'theme_location' => 'header-menu','container' => 'ul', 'menu_class' => 'nav'));
+    	}
+    	echo '</div>';
+    }
+?>
+
+    <div id="wrap">
+      <div id="preheader"></div>
+      <header id="header" class="clearfix">
+  		<div class="head">
+  			<h1><a href="<?=$home_url?>"><?php bloginfo('name')?></a></h1>
+  			<p class="desc yahei">
+                <span class="<?php echo dopt('d_saying_title')?'saying-title':''?>">
+                    <?php bloginfo('description')?>
+                </span>
+            </p>
+  		</div>
+
+            <?php
+                if( !is_mobile() ) {
+                    echo '<nav id="main-nav" class="clearfix yahei">';
+                    if(function_exists('wp_nav_menu')) {
+                        wp_nav_menu(array( 'theme_location' => 'header-menu','container' => 'ul', 'menu_class' => 'nav'));
+                    }
+                    echo '</nav>';
+                } else {
+                  echo '<a href="javascript:;" class="open-nav icon-list"><i class="fa fa-bars"></i></a>';
+                }
+            ?>
+
+            <div id="announcement">
+                <span><i class="fa fa-star"></i></span>
+            </div>
+        </header>
---
+++ inc/post-format/content.php
@@ -0,0 +1,52 @@
+<article <?php post_class(); ?>>
+    <header class="entry-header">
+        <h2 class="entry-name">
+            <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+        </h2>
+        <ul class="entry-meta">
+            <li class="time_meta"><i class="fa fa-clock-o"></i> <?php the_time('Y-m-d H:i');?></li>
+      <?php if( dopt('d_showcategory_b')!="" ) : ?>
+               <li class="cat_meta"><i class="fa fa-pencil-square-o"></i> <?php the_category(','); ?></li>
+      <?php endif; ?>
+            <li class="comments_meta"><i class="fa fa-comments-o"></i> <?php
+        if(comments_open())
+          comments_popup_link('暂无评论', '1 条评论', '% 条评论');
+        else {
+          echo '<a href="';
+          the_permalink();
+          echo '">评论关闭</a>';
+        }
+      ?></li>
+            <li class="views_meta"><i class="fa fa-eye"></i> <a><?php mzw_post_views(' 访问量');?></a></li>
+        </ul>
+    </header>
+    <div class="entry-content" itemprop="description">
+      <?php
+    		$pc = $post->post_content;
+    		$st = strip_tags(apply_filters('the_content',$pc));
+    		if(has_excerpt())
+    			the_excerpt();
+    		elseif( preg_match('/<!--more.*?-->/',$pc) || mb_strwidth($st)<500 )
+    			the_content('');
+    		else
+    			echo mb_strimwidth($st,0,500,' ......');
+    	?>
+    </div>
+    <footer class="entry-footer clearfix">
+    <span class="tag-links in-list"><?php the_tags( '', '', '' ); ?></span>
+        <div class="post-more">
+            <a href="<?php the_permalink(); ?>">阅读全文</a>
+        </div>
+    <?php if( dopt('d_ding_b') != '' ) : ?>
+        <div class="post-love">
+            <a href="javascript:;" data-action="ding" data-id="<?php the_ID(); ?>" class="favorite post-love-link <?php if(isset($_COOKIE['mzw_ding_'.$post->ID])) echo ' done';?>" title="点个赞"><i class="fa fa-heart-o"></i>
+            <span class="love-count">
+                <?php
+                  $ding_num = get_post_meta($post->ID,'mzw_ding',true);
+                  echo $ding_num? $ding_num : '0';
+                ?>
+            </span></a>
+        </div>
+    <?php endif; ?>
+    </footer>
+</article>
